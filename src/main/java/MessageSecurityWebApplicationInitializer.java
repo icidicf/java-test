e7d9692de5d986b9c4daed5c7b0faf006d930ea2
hzlouyangping <hzlouyangping@corp.netease.com>
2016-08-08 14:33:02 +0800

    upate web initial

---
+++ src/main/java/MessageSecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/**
+ * Created by lyp on 2016/8/8.
+ */
+public class MessageSecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer{
+}
